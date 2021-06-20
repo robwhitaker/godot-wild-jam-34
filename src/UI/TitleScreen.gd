604cd22c7db22294aa4cc6b5a8fc572457a29bbc
Rob Whitaker <dev@robwhitaker.com>
2021-06-20 16:14:08 -0400

    Fix title and game over screens

--- src/UI/TitleScreen.gd
+++ src/UI/TitleScreen.gd
@@ -14,6 +14,8 @@ func _ready():
 func _unhandled_input(event):
     if event.is_action_released("ui_accept"):
         get_tree().change_scene(next_scene_path)
+    elif event.is_action_pressed("ui_cancel"):
+        get_tree().quit()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
