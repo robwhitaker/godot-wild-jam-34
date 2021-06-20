968a273b408ccc28bc07a9d882b618ca87c656cd
Rob Whitaker <dev@robwhitaker.com>
2021-06-20 14:00:49 -0400

    Add carrot spawn to cargo ship

--- src/Enemies/cargoship.gd
+++ src/Enemies/cargoship.gd
@@ -20,9 +20,25 @@ func apply_damage(dmg : float) -> void:
     if enemy_hp <= 0.0:
         var root : Spatial = Utils.get_scene_root() as Spatial
         var explosion : Particles = preload("res://src/Explosion/Explosion.tscn").instance()
+
         explosion.global_transform = global_transform
         explosion.emitting = true
         root.add_child(explosion)
+
+        _spawn_carrots(15)
+
         queue_free()
     else:
         animation_player.play("Shake")
+
+func _spawn_carrots(amt : int) -> void:
+    while amt > 0:
+        var direction := Vector3.ZERO
+        direction.x = rand_range(-1, 1)
+        direction.y = rand_range(-1, 1)
+        direction.z = rand_range(0, 1)
+        var carrot := preload("res://src/Carrot/Carrot.tscn").instance() as Spatial
+        carrot.global_transform = global_transform
+        carrot.velocity = direction * 70
+        Utils.get_scene_root().add_child(carrot)
+        amt -= 1
